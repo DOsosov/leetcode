a66a45a0399821953f626e1bc178ce75faf29054
DOsosov <dimaososov@mail.ru>
2023-04-07 22:40:06 +0500

    35

---
+++ 35. Search Insert Position.go
@@ -0,0 +1,20 @@
+package main
+
+func searchInsert(nums []int, target int) int {
+    if nums[0] == target {
+        return 0
+    }
+    if nums[len(nums)-1] < target {
+        return len(nums)
+    }
+    for i := 1; i <= len(nums)-1; i++ {
+        if nums[i] == target {
+            return i
+        }
+        if (target < nums[i]) && (target > nums[i-1]) {
+            return i
+        }
+        
+    }
+    return 0
+}
