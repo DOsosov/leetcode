3d87da6ed3af6dc4be8a3929faa5737baa637108
DOsosov <dimaososov@mail.ru>
2023-04-15 22:05:19 +0500

    исправил

--- 9. Palindrome Number.go
+++ 9. Palindrome Number.go
@@ -1,16 +1,22 @@
 package main
+
+import "strconv"
+
 func isPalindrome(x int) bool {
-    var pruff bool = true
-    conv := []byte(strconv.Itoa(x))
-    for i := 0; i < len(conv)/2; i++ {
-        conv[i], conv[len(conv) - 1 - i] = conv[len(conv) - 1 - i], conv[i]
-    }
-    convobr := []byte(strconv.Itoa(x))
-    for i := 0; i < len(convobr); i++ {
-        if convobr[i] != conv[i] {
-            pruff = false
-            return pruff
+	var pruff bool = true
+	conv := []byte(strconv.Itoa(x))
+	for i := 0; i < len(conv)/2; i++ {
+		conv[i], conv[len(conv)-1-i] = conv[len(conv)-1-i], conv[i]
+	}
+
+	convobr := []byte(strconv.Itoa(x))
+	for i := 0; i < len(convobr); i++ {
+		if convobr[i] != conv[i] {
+			pruff = false
+
+			return pruff
         }
-    }
-    return pruff
+	}
+
+	return pruff
 }
