e71ee910300e4e9ed5f4f7461fc0d411f4e8ed50
DOsosov <dimaososov@mail.ru>
2023-04-15 22:00:29 +0500

    исправил

--- 66. Plus One.go
+++ 66. Plus One.go
@@ -1,8 +1,10 @@
 package main
+
 func main() {
 	digits := []int{1, 2, 3}
 	plusOne(digits)
 }
+
 func plusOne(digits []int) []int {
 	digits[len(digits) - 1] = digits[len(digits) - 1] + 1
 	for i := 1; i < len(digits); i++ {
@@ -11,11 +13,12 @@ func plusOne(digits []int) []int {
 			digits[len(digits) - i - 1] = digits[len(digits) - i - 1] + 1
 		}
 	}
+	
 	if digits[0] == 10 {
 		digits[0] = 0
 		digits2 := []int{1}
 		for i := 0; i < len(digits); i++ {
-		 digits2 = append(digits2, digits[i])
+			digits2 = append(digits2, digits[i])
 		}
 		return digits2
 	}
